2e9a78542d590475125643d49cbcfec17c6d1fb2
Darlene Matos <darlenem19@gmail.com>
2022-09-07 21:20:55 -0400

    Solved

---
+++ Level 8/oppositeNumber.js
@@ -0,0 +1,22 @@
+//P- an integer/floating point number 
+
+//R- output should be the opposite of that number 
+
+//E
+//1: -1
+//14: -14
+//-34: 34
+
+//P
+//
+
+//brainstorm- is there a way to check if a number is positive or negative 
+
+function opposite(number) {
+    
+ return number * -1 
+
+  }
+
+
+  console.log(opposite(-6))
