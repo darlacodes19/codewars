41b430bd3f9f8315fe38c22c37d770c7011124d8
Darlene Matos <darlenem19@gmail.com>
2022-09-20 15:35:30 -0400

    Solved using the filter method

--- Level6/arrayDiff.js
+++ Level6/arrayDiff.js
@@ -1,5 +1,5 @@
 //P- two arrays - numbers(values) or emoty- array 1= a ; array 2 = b 
-//R- array a without the values in array 6
+//R- array a without the values in array b
 //E arrayDiff([1,2,2,2,3],[2]) == [1,3]
 //P 
 
@@ -26,17 +26,10 @@ const newArray = []
 
 function arrayDiff2(a, b) {
 
-    const newArray = []
     
-        for( let i = 0; i <a.length; i++) {
-            for(let j = 0; j <b.length; j++) {
-                if(a[i] !== b[j]) {
-                    newArray.push(a[i])
-                }
-            }
-        }
+
+    return a.filter(number => !b.includes(number))
     
-        return newArray
     }
     
     console.log(arrayDiff2([1,2,2,2,3],[2])) 
