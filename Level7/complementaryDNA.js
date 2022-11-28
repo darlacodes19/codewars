01d22ed6e03fb1808ac2986b945fdfc13e91c3b6
Darlene Matos <darlenem19@gmail.com>
2022-11-28 13:34:25 -0500

    Solved

---
+++ Level7/complementaryDNA.js
@@ -0,0 +1,40 @@
+// P a string of letters ( A, T, G ,C)
+// R - return a string with the complementary side 
+// E - "ATTGC" --> "TAACG"
+        //"GTAT" --> "CATA"
+// P 
+
+//loop over the string 
+//use and if/else statement to switch 
+
+//AC => TG
+
+
+function DNAStrand(dna){
+
+    //A 
+    //T
+    //C
+    //G
+
+    let complementaryString = ""
+    
+    for ( let i= 0 ; i< dna.length; i++) {
+
+        if (dna[i] === 'A')  {
+            complementaryString += "T"
+        } else if (dna[i] === "T") {
+            complementaryString += "A"
+        }else if (dna[i] === "C") {
+            complementaryString += "G"
+        } else {
+            complementaryString += "C"
+        }
+    }
+
+    return complementaryString
+    
+  }
+
+  // "TAACG"
+  console.log(DNAStrand("ATTGC"))
---
+++ Level8/converNumberToString.js
@@ -0,0 +1,16 @@
+// Write a function that takes a number and and turns it into a string 
+
+//123  --> "123"
+//999  --> "999"
+//-100 --> "-100"
+
+
+function numberToString(num) {
+   
+    return num.toString()
+
+
+  }
+
+
+  console.log(numberToString(25))
