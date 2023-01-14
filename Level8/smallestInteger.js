dbfd809d9096c374aaf1c6660eb548dd1c61edd5
Darlene Matos <darlenem19@gmail.com>
2023-01-14 12:45:15 -0500

    Found the smallest integer in an array using Math.min and the spread operator

---
+++ Level8/smallestInteger.js
@@ -0,0 +1,17 @@
+//Given an array of integers your solution should find the smallest integer.
+
+
+//Solved using Math.min fucntion
+function findSmallest(array) {
+
+    return Math.min(...array)
+}
+
+console.log(findSmallest([1,3,5,6]))
+
+
+//solve using sort
+
+
+
+//solve using for loop 
